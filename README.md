a3316b7a354fa4b70c06dfc52a663661c620a394
hansa-lallu <hansalallu@gmail.com>
2020-05-03 05:42:47 +0100

    two routes. / and /users

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tasks
+
+1. create a Node.js driven Serve on port 3000
+
+2. Handle two routes . "/" and "/users. Return some greeting text on "/". Return a list of dummy users.
+3. Add a form with a "username" input to the "/" page and submit a POST request to "/create-user" upon a button clock
+4. Add the "/create-user" route and parse the incoming data (i.e. the username) and simply log it to the console. 
--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<head><title><Assignment 1</title><head>');
+      res.write('<body><p>Welcome to my page!</p><body>')
+      res.write('</html');
+      return res.end();
+  }
+  if (url === "/users") {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title><Assignment 1</title><head>');
+    res.write('<body><ul><li>User 1</li><li>User 2</li></ul><body>')
+    res.write('</html');
+    return res.end();
+  }
+   
 });
 
 server.listen(3000);
